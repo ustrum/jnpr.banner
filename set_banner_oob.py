c6aa0cddfe164a9ab47fbfc8203652de9f6eead6
ustrum <info@pablo-martin.net>
2018-11-14 15:25:46 +0100

    Mod set_banner_oob.py

--- set_banner_oob.py
+++ set_banner_oob.py
@@ -3,7 +3,7 @@ from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 
 
-OOB_SW_LIST = { "ODI":'10.254.3.110',"OAC01":'10.254.3.111',"OAC02":'10.254.3.112',"OAC03":'10.254.3.113',"OAC04":'10.254.3.114',"OAC05":'10.254.3.115',"OAC06":'10.254.3.116',"OAC07":'10.254.3.117',"OAC08":'10.254.3.118}
+OOB_SW_LIST = { "ODI":'10.254.3.110',"OAC01":'10.254.3.111',"OAC02":'10.254.3.112',"OAC03":'10.254.3.113',"OAC04":'10.254.3.114',"OAC05":'10.254.3.115',"OAC06":'10.254.3.116',"OAC07":'10.254.3.117',"OAC08":'10.254.3.118'}
 
 conf='set system login message "Remember to use your NGAHRUSER account to log in.\n\n"'
 
