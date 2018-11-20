ee98e1020e9ca76e041f77f7c79f358f3944f170
ustrum <info@pablo-martin.net>
2018-11-20 10:13:50 +0100

    updated functions

--- create_vlans.py
+++ create_vlans.py
@@ -1,9 +1,40 @@
 
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
+import ipaddress
 
 PLE_SW_LIST={"PLE01":{'IP':'10.254.3.102','HOS_USER_PORTS':['ae0','ae1'],'NFS_HOS_PORTS':['ae38','ae39','ae42','ae43','ae46','ae47'],'NFS_DMZ_PORTS':['ae38','ae39','ae42','ae43','ae46','ae47'],'DMZ_USER_PORTS':['ae2','ae3']},"PLE51":{'IP':'10.254.3.103','HOS_USER_PORTS':['ae0','ae1'],'NFS_HOS_PORTS':['ae38','ae39','ae42','ae43','ae46','ae47'],'NFS_DMZ_PORTS':['ae38','ae39','ae42','ae43','ae46','ae47'],'DMZ_PORTS':['ae2','ae3']},"PLE02":{'IP':'10.254.3.104','HOS_USER_PORTS':['ae12','ae13','ae14','ae15','ae16','ae17','ae18','ae19','ae20','ae21','ae22','ae23','ae24','ae25','ae26','ae27','ae28','ae29'],'NFS_HOS_PORTS':['ae30','ae31','ae32','ae33','ae34','ae35','ae','ae37','ae38','ae39','ae40','ae41','ae42','ae43','ae44','ae45','ae46','ae47'],'NFS_DMZ_PORTS':[],'DMZ_USER_PORTS':[]},"PLE52":{'IP':'10.254.3.105','HOS_USER_PORTS':['ae12','ae13','ae14','ae15','ae16','ae17','ae18','ae19','ae20','ae21','ae22','ae23','ae24','ae25','ae26','ae27','ae28','ae29'],'NFS_HOS_PORTS':['ae30','ae31','ae32','ae33','ae34','ae35','ae36','ae37','ae38','ae39','ae40','ae41','ae42','ae43','ae44','ae45','ae46','ae47'],'NFS_DMZ_PORTS':[],'DMZ_USER_PORTS':[]},"PLE03":{'IP':'10.254.3.106','HOS_USER_PORTS':[],'NFS_HOS_PORTS':[],'NFS_DMZ_PORTS':['ae28','ae29'],'DMZ_USER_PORTS':['ae24','ae25']},"PLE53":{'IP':'10.254.3.107','HOS_USER_PORTS':[],'NFS_HOS_PORTS':[],'NFS_DMZ_PORTS':['ae28','ae29'],'DMZ_USER_PORTS':['ae24','ae25']}}
 
+
+"""
+
+set security zones security-zone MONITORING host-inbound-traffic system-services ping
+set security zones security-zone MONITORING host-inbound-traffic system-services ntp
+set security zones security-zone MONITORING interfaces reth0.1704
+set interfaces reth0 unit 1704 description "*** MONITORING ***"
+set interfaces reth0 unit 1704 vlan-id 1704
+set interfaces reth0 unit 1704 family inet address 172.21.176.1/26
+set routing-instances CORE interface reth0.1704
+
+"""
+
+
+def create_zone_ifw(vlan_id,subnet,zone_name, int_desc=None, verbose=False):
+	config=[]
+	set_conf=''
+	int_ip=ipaddress.ip_network(subnet).network_address+'/'+subnet.prefixlen
+	config.append("set security zones security-zone " + zone_name + " host-inbound-traffic system-services ping")
+	config.append("set security zones security-zone " + zone_name + " host-inbound-traffic system-services ntp")
+	for host_int in FW_LIST['IFW']['HOS_USER_PORTS']:
+		config.append("set security zones security-zone " + zone_name + " host-inbound-traffic interfaces " + host_int + "." + vlan_id)
+	config.append("set interfaces " + host_int + " unit " + vlan_id  + " vlan-id " + vlan_id)
+	if int_desc: config.append("set interfaces " + host_int + " unit " + vlan_id  + " description " + int_desc)
+	config.append("set interfaces " + host_int + " unit " + vlan_id  + " family inet address " + int_ip)
+	config.append("set routing-instances CORE interface " + host_int + "." + vlan_id)
+	if verbose: print(config)
+	for conf_line in config:
+		set_conf+=conf_line+'\n'
+	return set_conf
 
 def create_user_hos_vlan_config(vlan_id,vlan_name):
 	config={}
